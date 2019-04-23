af0d188860c352964e71ffe879dad72cc2f26f5d
Lucas Araújo Pena <lucaspena4@gmail.com>
2019-04-23 20:57:51 -0300

    is this going to end someday?

--- Simulador RISCV/main.c
+++ Simulador RISCV/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h> 
 #include <stdint.h>
 #include <string.h>
+#include <inttypes.h>
 
 //Limpa o console
 #define CLEAR() system("clear || cls")
@@ -16,14 +17,38 @@
 #define MEM_SIZE 4096
 int32_t mem[MEM_SIZE];
 
+int32_t dump_add, dump_size;
+
 int is_running = 1;
 int op;
 
-//Imprime o conteudo da memoria, em formato hexadecimal.
-void dump_mem(uint32_t add, uint32_t size) {
-	// Ex: mem[0] = 01020304
+//Imprime o conteudo da memoria, em formato hexadecimal
+void dump_mem(uint32_t add, uint32_t wsize) {
+	//Variavel para corrigir a contagem do index da memoria
+	int a = add / 4;
+
+	for (unsigned int i = 0; i < MEM_SIZE; i++) {
+
+		//Coloca em tmp o valor da memoria
+		uint32_t tmp = mem[add / 4];
+
+		//Enquanto estiver no range de busca, printa na tela o valor
+		if (i <= wsize) {
+			printf("mem[%d] = 0x%08x \n", (i + a), tmp);
+		}
+
+		//Vai para o proximo endereco de memoria
+		add += 4;
+	}
 }
 
+//Garante que a memoria esteja vazia para realizar a inicializacao.
+//Utilizado em uma segunda chamada da funcao de inicializacao.
+void clean_mem() {
+	for (int i = 0; i < MEM_SIZE; i++) {
+		mem[i] = 0;
+	}
+}
 
 /*						Intrucoes						*/
 
@@ -31,24 +56,25 @@ void dump_mem(uint32_t add, uint32_t size) {
 int32_t lw(uint32_t add, int16_t kte) {
 	
 	//Verifica se o endereco nao eh multiplo de 4
-	if (add % 4) {
-		printf("Endereco invalido (lw)");
+	if (add % 4 != 0) {
+		printf("Endereco invalido. (Erro: lw)");
 		return;
 	}
 
-	return mem[add >> 2];
+	//Retorna a palavra 
+	return mem[(add + kte) / 4];
 }
 
 //Le meia palavra (16 bits). Retorna inteiro com sinal
 int32_t lh(uint32_t add, int16_t kte) {
-	uint32_t tmp = mem[add >> 2];
+	uint32_t tmp = mem[(add + kte) / 4];
 	uint32_t ret = 0;
 
 	if (add % 2) {
 		printf("\n\n Dados nao alinhados. (Erro: lh)");
 	}
 
-	if (add % 4) {
+	if (kte % 4 == 0) {
 		ret = (tmp >> 16) & 0xFFFF;
 	} else {
 		ret = tmp & 0xFFFF;
@@ -61,17 +87,17 @@ int32_t lh(uint32_t add, int16_t kte) {
 uint32_t lhu(uint32_t add, int16_t kte) {
 
 	//Avanca na memoria para o endereco
-	uint32_t tmp = mem[add << 2];
+	uint32_t tmp = mem[(add + kte) / 4];
 	
 	//Retorno da funcao
 	uint32_t ret = 0; 
 
 	// Checa se os Dados estao alinhados
-	if (add % 2) {
+	if (kte % 2 != 0) {
 		printf("\n\nDados nao alinhados. (Erro: lhu)");
 	}
 
-	if (!add%4) {
+	if (add % 4 == 0) { //!
 		ret = (tmp >> 16) & 0xFFFF;
 	}
 	else {
@@ -83,106 +109,308 @@ uint32_t lhu(uint32_t add, int16_t kte) {
 
 //Le um byte (8 bits) e retorna um inteiro com sinal
 int32_t lb(uint32_t add, int16_t kte) {
+	uint32_t tmp;
 
+	//Acessa o byte
+	tmp = (mem[(add + kte) / 4] >> (8 * (kte % 4)));
+	
+	//Acessa o bit
+	tmp &= 0xFF;
+
+	return ((tmp & 0x80) ? (tmp | 0xFFFFFF00) : tmp);
 }
 
 //Le um byte (8 bits) e retorna um inteiro sem sinal
 int32_t lbu(uint32_t add, int16_t kte) {
 	uint32_t tmp;
 
-	tmp = mem[add >> 2];
-	return (tmp >> (8 * (add % 4))) && 0xFF;
+	tmp = mem[(add + kte) / 4];
+	tmp = tmp >> (8 * kte);
+	tmp &= 0xFF;
+
+	return tmp;
+
+	//return (tmp >> (8 * (add % 4))) && 0xFF;
 }
 
 //Escreve um inteiro alinhado na memoria. Enderecos multiplos de 4
 void sw(uint32_t add, int16_t kte, int32_t dado) {
 
 	//Verifica se a constante possui um valor aceitavel
-	if (kte % 4) {
+	if ((add + kte) % 4 != 0) {
 		printf("\n\n A constante precisa ser um multiplo de 4. (Erro: sw)");
 	}
 
 	//Salva na memoria o dado
-	mem[dado >> 2] = dado;
+	mem[(add + kte) / 4] = dado;
 }
 
 //Escreve meia palavra de 16 bits na memoria. Enderecos multiplos de 2
 void sh(uint32_t add, int16_t kte, int16_t dado) {
 
-	// Vai para o proximo endereco de memoria
-	uint32_t tmp = mem[add >> 2 + (kte/4)];
+	// Endereco de memoria
+	uint32_t tmp = mem[(add + kte) / 4];
+
+	//Offset da meia palavra em relacao ao endereco da palavra
+	int hadd = (add + kte) % 4;
 
 	//Mascara apicada para poder pegar somente a meia palavra
 	dado &= 0xFFFF;
 
-	if (add % 2) {
+	if ((add + kte) % 2 != 0) {
 		printf("\n\nEndereco invalido. (Erro: sh)");
 		return;
 	}
-	if (add % 4) {
-		tmp &= 0x0000FFFF;
-		tmp |= dado << 16;
-	}
-	else {
+
+	if ((add + kte) % 4 == 0) {
 		tmp &= 0xFFFF0000;
 		tmp |= dado;
 	}
+	else {
+		tmp &= 0x0000FFFF;
+		tmp |= dado << 16;
+	}
 
 	//Salva a operacao na memoria
-	mem[add >> 2 + (kte / 4)] = tmp;
+	mem[(add + kte) / 4] = tmp;
 }
 
 //Escreve um byte de 8 bits na memoria.
 void sb(uint32_t add, int16_t kte, int8_t dado) {
-	uint32_t tmp = mem[add >> 2];
-	uint32_t off = add % 4;
-	int8_t exbyte = dado & 0xFF;
-	int masks[] = { 0xFF000000, 0xFF0000, 0xFF00, 0xFF };
+	//Carrega para tmp o valor que esta na memoria
+	uint32_t tmp = mem[(add + kte) / 4];
 
-	tmp = (tmp & masks[off]) + (exbyte << (8 * off));
-	mem[add >> 2] = tmp;
+	//Pega somente o dado fornecido
+	int8_t exbyte = dado & 0xFF;
+
+	//Array contendo as mascaras necessarias para a operacao
+	int mask[] = { 0xFF, 0xFF00, 0xFF0000, 0xFF000000 };
+
+	//Coloca o byte no local correto em relacao ao bit original
+	uint32_t a = exbyte << (8 * kte);
+	uint32_t b = a & mask[kte];
+
+	//Adiciona o byte ao bit
+	tmp += b;
+
+	//Salva na memoria o dado
+	mem[(add + kte) / 4] = tmp;
 }
 
 /*                              Funcoes auxiliares                              */
 
 //Limpa o buffer de input 
-void clean() {
+void clean_buffer() {
 	while (getchar() != '\n');
 }
 
+//Funcao para pausar o programa e esperar o enter para continuar
+void enter() {
+	printf("\n\nPressione Enter para continuar...");
+	clean_buffer();
+}
+
+//Printa na tela o menu principal para navegacao no programa
 void menuInicial() {
 	printf("Acesso a memoria RISC V \n\n");
 	printf("Escolha a opcao desejada:\n");
-	printf("1- Opcao\n");
-	printf("2- Opcao\n");
-	printf("3- Sair\n\n");
+	printf("1- Inicializar a memoria \n");
+	printf("2- Imprimir o conteudo da memoria\n");
+	printf("3- Ler os dados da memoria\n");
+	printf("4- Sair\n\n");
 }
 
-void opcao1() {}
-void opcao2() {}
+//Printa tela da opcao 'dump_mem()'
+void dump_memMenu() {
+	printf("Dump de memoria. \n\n");
+	printf("Entre com o endereco inicial (4096 max): ");
+
+	scanf("%" SCNd32, &dump_add);
+	clean_buffer();
+
+	//Testa se o endereco esta dentro do permitido
+	while (dump_add < 0 || dump_add > 4096 ) {
+		CLEAR();
+		printf("Entre com um endereco inicial valido (4096 max): ");
+
+		scanf("%" SCNd32, &dump_add);
+		clean_buffer();
+	}
+
+	printf("Entre com o a quantidade de palavras que deseja retornar (4096 max): ");
+
+	scanf("%" SCNd32, &dump_size);
+	clean_buffer();
+
+	//Testa se o tamanho esta dentro do permitido
+	while (dump_size < 0 || dump_size > (4096 - (int)dump_add) ) {
+		CLEAR();
+		printf("Entre com a quantidade de palavras validas (4096 max): ");
+
+		scanf("%" SCNd32, &dump_size);
+		clean_buffer();
+	}
+
+}
+
+//Inicializa a memoria com valores previamente estabelecidos
+void initMem() {
+
+	clean_mem();
+
+	// A
+	sb(0, 0, 0x04);
+	sb(0, 1, 0x03);
+	sb(0, 2, 0x02);
+	sb(0, 3, 0x01);
+
+	// B
+	sb(4, 0, 0xFF);
+	sb(4, 1, 0xFE);
+	sb(4, 2, 0xFD);
+	sb(4, 3, 0xFC);
+
+	// C
+	sh(8, 0, 0xFFF0);
+	sh(8, 2, 0x8C);
+
+	// D
+	sw(12, 0, 0xFF);
+
+	// E
+	sw(16, 0, 0xFFFF);
+
+	// F
+	sw(20, 0, 0xFFFFFFFF);
+
+	//G
+	sw(24, 0, 0x80000000);
+
+}
+
+//Le a memoria
+void read_mem() {
+	//Variaveis
+	int32_t a;
+
+	// A
+	a = lb(0, 0);
+	printf("Hex: 0x%02x  Dec: %d \n", a, a);
+	a = lb(0, 1);
+	printf("Hex: 0x%02x  Dec: %d \n", a, a);
+	a = lb(0, 2);
+	printf("Hex: 0x%02x  Dec: %d \n", a, a);
+	a = lb(0, 3);
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+
+	// B
+	a = lb(4, 0);
+	printf("Hex: 0x%02x  Dec: %d \n", a, a);
+	a = lb(4, 1);
+	printf("Hex: 0x%02x  Dec: %d \n", a, a);
+	a = lb(4, 2);
+	printf("Hex: 0x%02x  Dec: %d \n", a, a);
+	a = lb(4, 3);
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+
+	// C
+	a = lbu(4, 0);
+	printf("Dec: %d \n", a);
+	a = lbu(4, 1);
+	printf("Dec: %d \n", a);
+	a = lbu(4, 2);
+	printf("Dec: %d \n", a);
+	a = lbu(4, 3);
+	printf("Dec: %d \n\n", a);
+
+	// D
+	a = lh(8, 0);
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+	a = lh(8, 2);
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+	
+	// E
+	a = lhu(8, 0);
+	printf("Dec: %d \n\n", a);
+	a = lhu(8, 2);
+	printf("Dec: %d \n\n", a);
+
+	// F
+	a = lw(12, 0);
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+	a = lw(16, 0);
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+	a = lw(20, 0); 
+	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
+
+}
 
 //Loop principal do prorama
 void run() {
+	menuInicial();
+
 	while (is_running) {
-		menuInicial();
-
 		scanf("%d", &op);
-		clean();
+		clean_buffer();
 
+		// Seleciona a opcao escolhida
 		switch (op) {
 		case 1:
-			opcao1();
+			//Inicializa a memoria
+			initMem();
+
+			//Limpa a tela
+			CLEAR();
+
+			menuInicial();
+			printf("\nMemoria inicializada com sucesso. \n\n");
+
 			break;
 		case 2:
-			opcao2();
+			//Limpa a tela
+			CLEAR();
+
+			//Mostra na tela o menu de dump_mem
+			dump_memMenu();
+
+			//Chama a funcao principal de dump_mem
+			dump_mem(dump_add, dump_size);
+
+			//Pausa para mostrar na tela o dump de memoria
+			enter();
+
+			//Limpa a tela
+			CLEAR();
+
+			menuInicial();
+
 			break;
+
 		case 3:
+			//Limpa a tela para mostrar os dados
+			CLEAR();
+
+			//Puxa da memoria os dados
+			read_mem();
+
+			//Pausa a aplicacao e espera pelo enter do usuario
+			enter();
+
+			//Limpa a tela novamente
+			CLEAR();
+
+			menuInicial();
+
+			break;
+
+		case 4:
+			//Sai do programa
 			is_running = 0;
 
 		default:
 			CLEAR();
 			menuInicial();
-			printf("Entre uma opcao valida.");
+			printf("\nEntre uma opcao valida\n\n");
 		}
 
 	}
@@ -193,6 +421,10 @@ void run() {
 // Funcao principal do programa
 int main() {
 
+	//Inicializa a memoria com zeros
+	clean_mem();
+
+	//Chama a funcao principal do programa
 	run();
 
 	return 0;
