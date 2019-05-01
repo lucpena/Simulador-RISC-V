b9a95953704a5082095d9ecfc8df557d76a8f522
Lucas Araújo Pena <lucaspena4@gmail.com>
2019-05-01 09:23:51 -0300

    Ah shit, here we go again.

--- Simulador RISCV/main.c
+++ Simulador RISCV/main.c
@@ -1,8 +1,12 @@
 /*	Universidade de Brasilia
-*	Departamento de Ciência da Computacao
+*	Departamento de Ciencia da Computacao
 *	Trabalho 1 - Simulador do Risc V
 *
-*	Lucas Araújo Pena - 13/0056162
+*	IDE: Visual Studio 2015
+*	Compilador: Padrao do VS2019 para C
+*	Sistema operacional: Windows 7
+*
+*	Lucas Araujo Pena - 13/0056162
 */
 
 #include <stdio.h> 
@@ -18,10 +22,48 @@
 #define MEM_SIZE 4096
 int32_t mem[MEM_SIZE];
 
+int32_t regs[32];
+
+/*********************************************************************************************************************/
+
+// lembrem que so sao considerados os 7 primeiros bits dessas constantes
+enum OPCODES { 
+	LUI = 0x37, AUIPC = 0x17, // atribui 20 bits mais significativos
+	ILType = 0x03, // Load type
+	BType = 0x63, // branch condicional
+	JAL = 0x6F, JALR = 0x67, // jumps - JAL formato UJ, JALR formato I
+	StoreType = 0x23, // store
+	ILAType = 0x13, // logico-aritmeticas com imediato
+	RegType = 0x33, // operacoes LA com registradores
+	ECALL = 0x73 // chamada do sistema - formato I
+};
+
+// campo auxiliar de 3 bits
+enum FUNCT3 {
+	BEQ3 = 0, BNE3 = 01, BLT3 = 04, BGE3 = 05, BLTU3 = 0x06, BGEU3 = 07, // branches
+	LB3 = 0, LH3 = 01, LW3 = 02, LBU3 = 04, LHU3 = 05, // loads
+	SB3 = 0, SH3 = 01, SW3 = 02, // stores
+	ADDSUB3 = 0, SLL3 = 01, SLT3 = 02, SLTU3 = 03, // LA com
+	XOR3 = 04, SR3 = 05, OR3 = 06, AND3 = 07, // registradores
+	ADDI3 = 0, ORI3 = 06, SLTI3 = 02, XORI3 = 04, ANDI3 = 07, // LA com
+	SLTIU3 = 03, SLLI3 = 01, SRI3 = 05 // imediatos
+};
+
+// campo auxiliar de 7 bits para as instrucoes SRLI/SRAI, ADD/SUB, SRL/SRA
+enum FUNCT7 {
+	ADD7 = 0, SUB7 = 0x20, SRA7 = 0x20, SRL7 = 0, SRLI7 = 0x00, SRAI7 = 0x20
+};
+
+/*********************************************************************************************************************/
+
 int32_t dump_add, dump_size;
+
+uint32_t ri, pc;
 
 int is_running = 1;
 int op;
+
+/*										Funcoes Principais										*/
 
 //Imprime o conteudo da memoria, em formato hexadecimal
 void dump_mem(uint32_t add, uint32_t wsize) {
@@ -51,12 +93,33 @@ void clean_mem() {
 	}
 }
 
-/*						Intrucoes						*/
+
+void fetch() {
+	ri = mem[pc >> 2];
+	pc += 4;
+}
+
+void decode() {
+
+}
+
+void execute() {
+
+};
+
+//Chama oa funcoes de execucao do programa
+void step() {
+	fetch();
+	decode();
+	execute();
+};
+
+/*											Intrucoes											*/
 
 //Le um inteiro alinhado. Enderecos multiplos de 4
 int32_t lw(uint32_t add, int16_t kte) {
 	
-	int32_t tmp = mem[(add + kte) / 4];
+	int32_t tmp = mem[add >> 2];
 
 	//Verifica se o endereco nao eh multiplo de 4
 	if (add % 4 != 0) {
@@ -70,7 +133,7 @@ int32_t lw(uint32_t add, int16_t kte) {
 
 //Le meia palavra (16 bits). Retorna inteiro com sinal
 int32_t lh(uint32_t add, int16_t kte) {
-	uint32_t tmp = mem[(add + kte) / 4];
+	uint32_t tmp = mem[add >> 2];
 	uint32_t ret = 0;
 
 	if (add % 2) {
@@ -92,7 +155,7 @@ uint32_t lhu(uint32_t add, int16_t kte) {
 	uint32_t tmp, ret;
 	
 	//Coloca em tmp o valor da memoria do endereco 
-	tmp = mem[(add + kte) / 4];
+	tmp = mem[add >> 2];
 	ret = 0; 
 
 	// Checa se os Dados estao alinhados
@@ -115,19 +178,19 @@ int32_t lb(uint32_t add, int16_t kte) {
 	uint32_t tmp;
 
 	//Acessa o byte
-	tmp = (mem[(add + kte) / 4] >> (8 * (kte % 4)));
+	tmp = (mem[add >> 2] >> (8 * (kte % 4)));
 	
 	//Acessa o bit
 	tmp &= 0xFF;
 
-	return ((tmp & 0x80 == 0) ? (tmp | 0xFFFFFF00) : tmp);
+	return (((tmp & 0x80) == 0) ? (tmp | 0xFFFFFF00) : tmp);
 }
 
 //Le um byte (8 bits) e retorna um inteiro sem sinal
 int32_t lbu(uint32_t add, int16_t kte) {
 	uint32_t tmp;
 
-	tmp = mem[(add + kte) / 4];
+	tmp = mem[add >> 2];
 	tmp = tmp >> (8 * kte);
 	tmp &= 0xFF;
 
@@ -143,14 +206,14 @@ void sw(uint32_t add, int16_t kte, int32_t dado) {
 	}
 
 	//Salva na memoria o dado
-	mem[(add + kte) / 4] = dado;
+	mem[add >> 2] = dado;
 }
 
 //Escreve meia palavra de 16 bits na memoria. Enderecos multiplos de 2
 void sh(uint32_t add, int16_t kte, int16_t dado) {
 
 	// Endereco de memoria
-	uint32_t tmp = mem[(add + kte) / 4];
+	uint32_t tmp = mem[add >> 4];
 
 	//Offset da meia palavra em relacao ao endereco da palavra
 	int hadd = (add + kte) % 4;
@@ -173,13 +236,13 @@ void sh(uint32_t add, int16_t kte, int16_t dado) {
 	}
 
 	//Salva a operacao na memoria
-	mem[(add + kte) / 4] = tmp;
+	mem[add >> 2] = tmp;
 }
 
 //Escreve um byte de 8 bits na memoria.
 void sb(uint32_t add, int16_t kte, int8_t dado) {
 	//Carrega para tmp o valor que esta na memoria
-	uint32_t tmp = mem[(add + kte) / 4];
+	uint32_t tmp = mem[add >> 2];
 
 	//Pega somente o dado fornecido
 	int8_t exbyte = dado & 0xFF;
@@ -195,7 +258,7 @@ void sb(uint32_t add, int16_t kte, int8_t dado) {
 	tmp += b;
 
 	//Salva na memoria o dado
-	mem[(add + kte) / 4] = tmp;
+	mem[add >> 2] = tmp;
 }
 
 /*                              Funcoes auxiliares                              */
@@ -257,153 +320,69 @@ void dump_memMenu() {
 	printf("\n");
 }
 
-//Inicializa a memoria com valores previamente estabelecidos
-void initMem() {
+void initMem(char *textbin, char *databin) {
 
-	clean_mem();
+	FILE* text, * data;
+	char byte;
+	int num = 0, i, word, auxb;
 
-	// A
-	sb(0, 0, 0x04);
-	sb(0, 1, 0x03);
-	sb(0, 2, 0x02);
-	sb(0, 3, 0x01);
+	for (i = 0; i < 32; i++) {
+		regs[i] = 0;
+	}
 
-	// B
-	sb(4, 0, 0xFF);
-	sb(4, 1, 0xFE);
-	sb(4, 2, 0xFD);
-	sb(4, 3, 0xFC);
+	regs[28] = 0x1800;
 
-	// C
-	sh(8, 0, 0xFFF0);
-	sh(8, 2, 0x8C);
+	//Stack Pointer
+	regs[29] = 0x3FFC;
 
-	// D
-	sw(12, 0, 0xFF);
+	//Abre o arquivo textbin
+	text = fopen(textbin, "rb+");
+	if (!text) {
+		printf("Arquivo %s nao econtrado. (Text Bin)", textbin);
+		enter();
+		return;
+	}
 
-	// E
-	sw(16, 0, 0xFFFF);
+	//Abre o arquivo de databin
+	data = fopen(databin, "rb+");
+	if (!data) {
+		printf("Arquivo %s nao encontrado. (Data Bin)", databin);
+		enter();
+		return;
+	}
 
-	// F
-	sw(20, 0, 0xFFFFFFFF);
+	//Percorre o arquivo Text
+	while (!feof(text) && num < (0x2000 >> 2)) {
+		word = 0;
+		for (i = 0; i < 4; i++)	{
+			if (1 == fread(&byte, sizeof(char), 1, text)) {
+				auxb = (int)byte & 0xFF;
+				word += auxb << (8 * i);
+			}
+		}
 
-	// G
-	sw(24, 0, 0x80000000);
+		mem[num] = word;
+		num++;
+	}
+	fclose(text);
 
-	//Testes criados alem dos fornecidos
-	// 1
-	sb(28, 0, 0xBA);
-	sb(28, 1, 0xBA);
-	sb(28, 2, 0xEF);
-	sb(28, 3, 0xDD);
+	num = (0x2000 >> 2);
 
-	// 2
-	sb(32, 0, 0x11);
-	sb(32, 1, 0x22);
-	sb(32, 2, 0xFF);
-	sb(32, 3, 0xEE);
-
-	// 3
-	sh(36, 0, 0xABBA);
-	sh(36, 2, 0xEF);
-
-	// 4
-	sw(40, 0, 0xFAFBFCFD);
-
-	// 5
-	sw(44, 0, 0x88CA);
+	//Percorre o arquivo data
+	while (!feof(data) && num < (0x8000 >> 2)) {
+		word = 0;
+		for (i = 0; i < 4; i++)	{
+			if (1 == fread(&byte, sizeof(char), 1, data)) {
+				auxb = (int)byte & 0xFF;
+				word += auxb << (8 * i);
+			}
+		}
+		mem[num] = word;
+		num++;
+	}
+	fclose(data);
 }
 
-//Le a memoria
-void read_mem() {
-	//Variaveis
-	int32_t a;
-
-	// A
-	a = lb(0, 0);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(0, 1);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(0, 2);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(0, 3);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-
-	// B
-	a = lb(4, 0);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(4, 1);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(4, 2);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(4, 3);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-
-	// C
-	a = lbu(4, 0);
-	printf("Dec: %d \n", a);
-	a = lbu(4, 1);
-	printf("Dec: %d \n", a);
-	a = lbu(4, 2);
-	printf("Dec: %d \n", a);
-	a = lbu(4, 3);
-	printf("Dec: %d \n\n", a);
-
-	// D
-	a = lh(8, 0);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-	a = lh(8, 2);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-	
-	// E
-	a = lhu(8, 0);
-	printf("Dec: %d \n\n", a);
-	a = lhu(8, 2);
-	printf("Dec: %d \n\n", a);
-
-	// F
-	a = lw(12, 0);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-	a = lw(16, 0);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-	a = lw(20, 0); 
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-
-	//Testes realizados alem dos fornecidos
-	// 1
-	a = lb(28, 0);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(28, 1);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(28, 2);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(28, 3);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-
-	// 2
-	a = lb(32, 0);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(32, 1);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(32, 2);
-	printf("Hex: 0x%02x  Dec: %d \n", a, a);
-	a = lb(32, 3);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-
-	// 3
-	a = lbu(36, 0);
-	printf("Dec: %d \n", a);
-	a = lbu(36, 1);
-	printf("Dec: %d \n", a);
-	a = lbu(36, 2);
-	printf("Dec: %d \n", a);
-	a = lbu(36, 3);
-	printf("Dec: %d \n\n", a);
-
-	// 4
-	a = lw(40, 0);
-	printf("Hex: 0x%02x  Dec: %d \n\n", a, a);
-}
 
 //Loop principal do prorama
 void run() {
@@ -417,7 +396,7 @@ void run() {
 		switch (op) {
 		case 1:
 			//Inicializa a memoria
-			initMem();
+			//initMem();
 
 			//Limpa a tela
 			CLEAR();
@@ -451,7 +430,7 @@ void run() {
 			CLEAR();
 
 			//Puxa da memoria os dados
-			read_mem();
+			//read_mem();
 
 			//Pausa a aplicacao e espera pelo enter do usuario
 			enter();
@@ -483,6 +462,9 @@ int main() {
 
 	//Inicializa a memoria com zeros
 	clean_mem();
+
+	//Inicializa o PC em zero
+	pc = 0;
 
 	//Chama a funcao principal do programa
 	run();
